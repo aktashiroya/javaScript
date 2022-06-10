42b621899749f8e13b74d4fe7d0993ae1d66c1d2
akta shiroya <akta8758@gmail.com>
2022-06-10 13:10:36 +0530

    add file

---
+++ js/coding_1.js
@@ -0,0 +1,32 @@
+
+// let js = 'amazing';
+
+// if (js === 'amazing') alert('javascript is fun!');
+
+// 60 + 68 * 28 - 85;
+// console.log(60 + 68 * 28 - 805);
+
+// console.log('jon');
+// console.log('34');
+
+// let firstname = "jonas";
+// console.log(firstname);
+// console.log(js);
+// let $new = "jon";
+
+// let javascriptFun = true;
+
+// console.log(typeof true);
+// console.log(typeof 67);
+// console.log(typeof 'jon');
+// console.log(typeof javascriptFun);
+// console.log(typeof null);
+
+// let age = 32;
+// const birthDate = 9091;
+
+//...jacascript operater...
+
+const agejon = 2022 - 2003;
+const agesara = 2022 - 2008;
+console.log(agejon, agesara);
